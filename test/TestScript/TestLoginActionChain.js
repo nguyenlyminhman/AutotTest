46c73927cb84201d7af3885d40d4b133aded90f8
Nguyen Ly Minh Man <nguyenlyminhman@gmail.com>
2019-05-01 14:41:35 +0700

    Add Test Data

--- test/TestScript/TestLoginActionChain.js
+++ test/TestScript/TestLoginActionChain.js
@@ -1,18 +1,47 @@
 let loginObj = require('../PageObject/LoginActionChain');
+let Data = require('../TestData/Login');
 
 describe('Page Object API Command', () => {
-    it('Login API', () => {
-        let username = 'tomsmith';
-        let password = 'SuperSecretPassword!'
-        let url = 'https://the-internet.herokuapp.com/login';
 
+    let url = 'https://the-internet.herokuapp.com/login';
+
+    before('Data Preparation', ()=>{
+        console.log('Before Suite');
+        loginObj.openUrl(url)
+    })
+
+    beforeEach('Refresh Page', ()=>{
+        console.log('Before Each');
+        browser.refresh();
+    });
+
+    afterEach('Clear Data', ()=>{
+        console.log('After Each');
+    });
+
+    it('Login API Fail', () => {
         loginObj
-            .openUrl(url)
-            .inputUsername(username)
-            .inputPassword(password)
+            .inputUsername(Data.inCorrectCredential.username)
+            .inputPassword(Data.inCorrectCredential.password)
             .clickOnLogin()
             .getSecureAreaText();
-
         browser.pause(3000);
     });
+    
+    it('Login API Pass', () => {
+        loginObj
+            .inputUsername(Data.correctCredential.username)
+            .inputPassword(Data.correctCredential.password)
+            .clickOnLogin()
+            .getSecureAreaText();
+        browser.pause(3000);
+    });
+
+    it('Test case 03', ()=>{
+        console.log('Test case 02');
+    })
+
+    after('Done', ()=>{
+        console.log('After')
+    })
 });
